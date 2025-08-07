29da3ea21719ec151134d355f65d2181a022b370
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2025-08-07 17:01:36 +0300

    react starter added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-red-600">Hi</h1>
+      <Button>Shadcn</Button>
+    </>
+  )
+}
+
+export default App
