3a7ae7d54bafde5dfd908f45a57a00d1d4f2e157
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2025-08-10 23:44:11 +0300

    pull down query logic to DailyHadith page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,45 +3,17 @@ import { Routes, Route } from "react-router";
 import DailyHadithPage from "./pages/DailyHadithPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import Navbar from "./components/Navbar";
-import { getRandomHadith } from "@/api/hadithApi";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { Spinner } from "@/components/ui/shadcn-io/spinner";
 
 function App() {
-  const queryClient = useQueryClient();
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["randomHadith"],
-    queryFn: getRandomHadith,
-    refetchOnWindowFocus: false,
-  });
-
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar />
-      {isLoading && (
-        <div className="p-4 border border-8 flex flex-col flex-1 items-center justify-center">
-          <Spinner />
-        </div>
-      )}
-      {isError && (
-        <div className="text-center p-4 text-red-500">
-          Error fetching hadith
-        </div>
-      )}
-
-      {data && (
-        <main className="p-4 border border-8 flex flex-col flex-1 items-center justify-center">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <DailyHadithPage hadith={data.hadith} source={data.source} />
-              }
-            />
-            <Route path="/Favorites" element={<FavoritesPage />} />
-          </Routes>
-        </main>
-      )}
+      <main className="p-4 border border-8 flex flex-col flex-1 items-center justify-center">
+        <Routes>
+          <Route path="/" element={<DailyHadithPage />} />
+          <Route path="/Favorites" element={<FavoritesPage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- frontend/src/pages/DailyHadithPage.tsx
+++ frontend/src/pages/DailyHadithPage.tsx
@@ -1,24 +1,44 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getRandomHadith } from "@/api/hadithApi";
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
 
 type DailyHadithPageProps = {
   hadith: string;
   source: string;
 };
 
-export default function DailyHadithPage({
-  hadith,
-  source,
-}: DailyHadithPageProps) {
+export default function DailyHadithPage() {
+  const queryClient = useQueryClient();
+  const { data, isFetching, isError } = useQuery({
+    queryKey: ["randomHadith"],
+    queryFn: getRandomHadith,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+  });
+  function handleRefresh() {
+    queryClient.invalidateQueries({
+      queryKey: ["randomHadith"],
+    });
+  }
+
+  //   const { data, isLoading, isError } = useQuery({
+  //     queryKey: ["randomHadith"],
+  //     queryFn: getRandomHadith,
+  //   });
   return (
     <div className="border-2 rounded-2xl border-black p-4 grid gap-3 w-2/3">
-      <h1>{hadith}</h1>
-      <h3>{source}</h3>
-      {/* <Link to="/Favorites">
-        <Button>goto Favorites</Button>
-      </Link> */}
+      {isError && <h1>Error loading hadith.</h1>}
+      {isFetching ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1>{data?.hadith}</h1>
+          <h3>{data?.source}</h3>
+        </>
+      )}
       <div className="buttons flex gap-1 justify-center">
-        <Button>Refresh</Button>
+        <Button onClick={handleRefresh}>Refresh</Button>
         <Button>Add to Favorite</Button>
       </div>
     </div>
